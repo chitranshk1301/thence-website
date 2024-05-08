912778ba10c1bb3092114c9f31696ff515f9729f
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2024-05-08 19:38:28 +0530

    added types props

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,7 +4,7 @@ const InputField = (props) => {
     return (
         <div>
             <input
-                type="text"
+                type={props.type}
                 className='w-[417px] h-[75px] rounded-[64px] transition-all px-10 bg-[#EFEFEF] focus:bg-[#F5F8FF] focus:border-[#537FF1] placeholder-gray-500'
                 placeholder={props.placeholder}
             />
